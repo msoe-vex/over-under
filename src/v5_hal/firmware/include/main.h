0440426646a29205c05343774d2574bf8e67c621
Alex Kempen <alex.bookreader@gmail.com>
2023-08-19 04:45:24 +0000

    Deleted battery node and competition status node

--- src/v5_hal/firmware/include/main.h
+++ src/v5_hal/firmware/include/main.h
@@ -50,8 +50,6 @@
 #include "nodes/sensor_nodes/InertialSensorNode.h"
 #include "nodes/subsystems/drivetrain_nodes/HolonomicDriveNode.h"
 #include "nodes/subsystems/drivetrain_nodes/TankDriveNode.h"
-#include "nodes/system_nodes/BatteryNode.h"
-#include "nodes/system_nodes/CompetitionStatusNode.h"
 #include "nodes/system_nodes/ConnectionCheckerNode.h"
 #include "pathing/PathManager.h"
 #include "util/Logger.h"
